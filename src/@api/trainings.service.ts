d8605d572a7c4a63bc96451889f9a70d9158794e
Christopeer <cguerrerob2@upao.edu.pe>
2023-11-27 18:39:58 -0500

    Add training logic

---
+++ src/@api/trainings.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, lastValueFrom } from 'rxjs';
+import { DataExercises } from './data-exercises.service';
+
+export interface Trainings {
+  id: number;
+  exercises: DataExercises[];
+  reps: string;
+  weight: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TrainingsService {
+  private apiUrl = 'http://localhost:8080/training/';
+
+  constructor(private http: HttpClient) {}
+
+  httpClient = inject(HttpClient)
+
+  async getTrainings() {
+    return lastValueFrom(this.httpClient.get<Trainings[]>(`${this.apiUrl}`));
+  }
+
+  // Guardar un nuevo entrenamiento
+  saveTraining(routine: Trainings): Observable<any> {
+    return this.http.post(`${this.apiUrl}`, routine);
+  }
+}
--- src/app/components/searchtraining/searchtraining.component.ts
+++ src/app/components/searchtraining/searchtraining.component.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import { SharedDataService } from 'src/@api/shared-data.service';
 import { AuthService } from 'src/@api/auth.service';
 import { DataExercisesService, DataExercises } from 'src/@api/data-exercises.service';
+import { Trainings, TrainingsService } from 'src/@api/trainings.service';
 import axios from 'axios';
 
 @Component({
@@ -19,14 +20,20 @@ export class SearchtrainingComponent implements OnInit {
     private http: HttpClient,
     private sharedDataService: SharedDataService,
     private auth: AuthService,
-    private dataExerciseService: DataExercisesService
+    private dataExerciseService: DataExercisesService,
+    private trainingsService: TrainingsService
   ) {}
+
+  // At the top of your component class
+  reps: string = '12'; // default value, you can set it based on your requirement
+  weight: string = '12'; // default value, you can set it based on your requirement
 
   userEmail: string | null = null;
   selectedExercise: Exercises | null = null;
 
   searchTrainingsApiService = inject(ExercisesTemplatesService);
   exercisesList = inject(DataExercisesService);
+  trainingsApiService = inject(TrainingsService);
   
   showAlert: boolean = false;
 
@@ -40,6 +47,8 @@ export class SearchtrainingComponent implements OnInit {
     this.exercisesTemplates = await this.searchTrainingsApiService.getListExercises();
 
     this.dataExercises = await this.exercisesList.getListExercises();
+
+    this.trainingData = await this.trainingsApiService.getTrainings();
   }
 
   indexReturn() {
@@ -65,6 +74,44 @@ export class SearchtrainingComponent implements OnInit {
     }, 2000); // Cambia el tiempo (en milisegundos) según tus necesidades
 
     console.log(this.routine);
+  }
+
+  trainingData: Trainings[] = []; // Asigna el tipo de datos adecuado si es posible
+
+  saveTraining(routine: DataExercises[]) {
+    // Crea un nuevo entrenamiento con la lista de ejercicios proporcionada
+    const newTraining: Trainings = {
+      id: 0, // O el valor adecuado si ya tienes una lógica para generar IDs
+      reps: this.reps, // Asigna los valores adecuados
+      weight: this.weight, // Asigna los valores adecuados
+      exercises: routine.map(exercise => ({
+        id: exercise.id || 0, // Asegúrate de manejar el caso donde el id pueda ser nulo
+        exercise_Name: exercise.exercise_Name || '',
+        description_URL: exercise.description_URL || '',
+        exercise_Image: exercise.exercise_Image || '',
+        exercise_Image1: exercise.exercise_Image1 || '',
+        muscle_gp_details: exercise.muscle_gp_details || '',
+        muscle_gp: exercise.muscle_gp || '',
+        equipment_details: exercise.equipment_details || '',
+        equipment: exercise.equipment || '',
+        rating: exercise.rating || '',
+        description: exercise.description || '',
+      })),
+    };
+  
+    // Guarda el nuevo entrenamiento
+    this.trainingsService.saveTraining(newTraining).subscribe(
+      (response) => {
+        console.log('Entrenamiento guardado:', response);
+        this.addTrainingSuccess = true;
+        setTimeout(() => {
+          this.router.navigate(['/training']);
+        }, 2000);
+      },
+      (error) => {
+        console.error('Error al guardar el entrenamiento:', error);
+      }
+    );
   }
 
   closeAlert() {
